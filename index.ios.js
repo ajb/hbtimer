37a4f68b41de9dd8b671451783bd671a37b296c6
Adam Becker <ad@mbecker.cc>
2017-04-12 14:24:00 -0700

    init

---
+++ index.ios.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+export default class hbtimer extends Component {
+  render() {
+    return (
+      <Text>Hi</Text>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: '#F5FCFF',
+  }
+});
+
+AppRegistry.registerComponent('hbtimer', () => hbtimer);
