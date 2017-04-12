738b92c99b63e15f322112e8db3a9d6fcb245651
Adam Becker <ad@mbecker.cc>
2017-04-12 15:36:06 -0700

    trying to setup the ui

--- index.ios.js
+++ index.ios.js
@@ -9,25 +9,76 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  FlatList
 } from 'react-native';
 
+import NavigationBar from 'react-native-navbar';
+
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+const plusIcon = (
+  <TouchableOpacity>
+    <Icon name="plus" size={24} color="#037ac7" style={{marginTop: 8, marginRight: 10}} />
+  </TouchableOpacity>
+)
+
+const dummyHangs = [
+  { key: 0, text: "3 finger large edge", weight: "20" },
+  { key: 1, text: "Sloper", weight: "30" }
+]
 
 export default class hbtimer extends Component {
   render() {
     return (
-      <Text>Hi</Text>
+      <View style={styles.container}>
+        <NavigationBar
+          title={{ title: "HBTimer" }}
+          leftButton={{ title: "Edit" }}
+          rightButton={plusIcon}
+          containerStyle={{borderBottomWidth: 1, borderBottomColor: "#ccc"}}
+        />
+
+        <View style={{flex: 1}}>
+          {dummyHangs.map((item, index) => <Text key={index}>{item.text}</Text>)}
+        </View>
+
+        <View style={{flexDirection: "row", justifyContent: "center", alignItems: "center", height: 100, padding: 1, borderTopWidth: 1, borderTopColor: "#ccc", backgroundColor: "white"}}>
+          <TouchableHighlight style={{
+            height: 80,
+            width: 80,
+            borderRadius: 50,
+            backgroundColor: "#007100"
+          }} underlayColor="#004f00"
+          onPress={() => {}}
+          >
+            <View style={{
+              borderColor: "white",
+              borderWidth: 1,
+              height: 76,
+              width: 76,
+              marginTop: 2,
+              marginLeft: 2,
+              borderRadius: 76/2,
+              justifyContent: "center",
+              alignItems: "center"
+            }}>
+              <Text style={{fontSize: 16, color: "#cbe6c4", backgroundColor: "transparent"}}>Start</Text>
+            </View>
+          </TouchableHighlight>
+        </View>
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // backgroundColor: '#F5FCFF',
-  }
-});
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "white"
+  },
+};
 
 AppRegistry.registerComponent('hbtimer', () => hbtimer);
