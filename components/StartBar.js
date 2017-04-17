249954b06acb4a0059f70ebd73e9c8fd9ed55b6c
Adam Becker <ad@mbecker.cc>
2017-04-17 16:53:50 -0700

    break out CircleButton component

--- components/StartBar.js
+++ components/StartBar.js
@@ -1,97 +1,52 @@
 import React from 'react'
-import { View, TouchableHighlight, Text } from 'react-native'
-
+import { View, TouchableHighlight, Text, StyleSheet } from 'react-native'
 import * as workoutStatuses from '../constants/workoutStatuses'
+import styles from '../styles'
 
+function CircleButton (props) {
+  return (
+    <TouchableHighlight
+      style={StyleSheet.flatten([
+        props.styles,
+        styles.circleButton,
+        { backgroundColor: props.backgroundColor }
+      ])}
+      underlayColor={props.underlayColor}
+      onPress={props.onPress}
+    >
+      <View style={styles.circleButtonInner}>
+        <Text style={StyleSheet.flatten([styles.circleButtonText, { color: props.textColor }])}>{props.children}</Text>
+      </View>
+    </TouchableHighlight>
+  )
+}
 
 export default function StartBar (props) {
   return (
-    <View style={{
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      height: 100,
-      padding: 1,
-      backgroundColor: "#000",
-      borderTopWidth: 1,
-      borderTopColor: "#444",
-      paddingLeft: 20,
-      paddingRight: 20
-    }}>
-      <TouchableHighlight style={{
-        height: 80,
-        width: 80,
-        borderRadius: 50,
-        backgroundColor: "#333",
-        opacity: props.workoutStatus === workoutStatuses.STOPPED ? 0.7 : 1
-      }} underlayColor="#222"
-      onPress={props.onCancel}
-      >
-        <View style={{
-          borderColor: "#000",
-          borderWidth: 1,
-          height: 76,
-          width: 76,
-          marginTop: 2,
-          marginLeft: 2,
-          borderRadius: 76/2,
-          justifyContent: "center",
-          alignItems: "center"
-        }}>
-          <Text style={{fontSize: 16, color: "#eee", backgroundColor: "transparent"}}>Cancel</Text>
-        </View>
-      </TouchableHighlight>
+    <View style={styles.startBar}>
+      <CircleButton
+        backgroundColor="#333"
+        underlayColor="#222"
+        textColor="#eee"
+        styles={{opacity: props.workoutStatus === workoutStatuses.STOPPED ? 0.7 : 1}}
+        onPress={props.onCancel}
+      >Cancel</CircleButton>
 
       {
-        props.workoutStatus === workoutStatuses.STARTED ? (
-          <TouchableHighlight style={{
-            height: 80,
-            width: 80,
-            borderRadius: 50,
-            backgroundColor: "#b36909"
-          }} underlayColor="#774709"
-          onPress={props.onPause}
-          >
-            <View style={{
-              borderColor: "#000",
-              borderWidth: 1,
-              height: 76,
-              width: 76,
-              marginTop: 2,
-              marginLeft: 2,
-              borderRadius: 76/2,
-              justifyContent: "center",
-              alignItems: "center"
-            }}>
-              <Text style={{fontSize: 16, color: "#decab6", backgroundColor: "transparent"}}>Pause</Text>
-            </View>
-          </TouchableHighlight>
-        ) : (
-          <TouchableHighlight style={{
-            height: 80,
-            width: 80,
-            borderRadius: 50,
-            backgroundColor: "#007100"
-          }} underlayColor="#004f00"
-          onPress={props.workoutStatus === workoutStatuses.PAUSED ? props.onResume : props.onStart}
-          >
-            <View style={{
-              borderColor: "#000",
-              borderWidth: 1,
-              height: 76,
-              width: 76,
-              marginTop: 2,
-              marginLeft: 2,
-              borderRadius: 76/2,
-              justifyContent: "center",
-              alignItems: "center"
-            }}>
-              <Text style={{fontSize: 16, color: "#cbe6c4", backgroundColor: "transparent"}}>
-                {props.workoutStatus === workoutStatuses.PAUSED ? 'Resume' : 'Start'}
-              </Text>
-            </View>
-          </TouchableHighlight>
-        )
+        props.workoutStatus === workoutStatuses.STARTED ?
+          <CircleButton
+            backgroundColor="#b36909"
+            underlayColor="#774709"
+            textColor="#decab6"
+            onPress={props.onPause}
+          >Pause</CircleButton>
+          :
+          <CircleButton
+            backgroundColor="#007100"
+            underlayColor="#004f00"
+            textColor="#cbe6c4"
+            onPress={props.workoutStatus === workoutStatuses.PAUSED ? props.onResume : props.onStart}
+          >{props.workoutStatus === workoutStatuses.PAUSED ? 'Resume' : 'Start'}</CircleButton>
       }
     </View>
   )
--- styles/index.js
+++ styles/index.js
@@ -3,8 +3,10 @@ import { StyleSheet } from 'react-native'
 export const colors = {
   orange: '#ff9600',
   darkGray: '#666',
+  darkerGray: '#444',
   darkestGray: '#222',
-  white: '#fff'
+  white: '#fff',
+  black: '#000'
 }
 
 let styles = StyleSheet.create({
@@ -28,6 +30,38 @@ let styles = StyleSheet.create({
   navbarPlus: {
     marginTop: 6,
     marginRight: 8
+  },
+  startBar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 100,
+    padding: 1,
+    backgroundColor: colors.black,
+    borderTopWidth: 1,
+    borderTopColor: colors.darkerGray,
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  circleButton: {
+    height: 80,
+    width: 80,
+    borderRadius: 50
+  },
+  circleButtonInner: {
+    borderColor: colors.black,
+    borderWidth: 1,
+    height: 76,
+    width: 76,
+    marginTop: 2,
+    marginLeft: 2,
+    borderRadius: 76/2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  circleButtonText: {
+    fontSize: 16,
+    backgroundColor: "transparent"
   }
 })
 
