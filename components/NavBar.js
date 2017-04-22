e742a3cc635d9030143c66b193f4d7fa8b46bded
Adam Becker <ad@mbecker.cc>
2017-04-21 17:41:11 -0700

    convert NavBar to a class so the debugger stops complaining about refs

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { TouchableOpacity } from 'react-native'
 import NavigationBar from 'react-native-navbar'
 import Icon from 'react-native-vector-icons/Entypo'
 import styles, { colors } from '../styles'
 
-export default function NavBar(props) {
-  return (
-    <NavigationBar
-      title={{ title: "Hangboard Timer", style: styles.navbarTitle }}
-      leftButton={
-        props.currentRoute === 'edit' ?
-          { title: "Done", handler: props.handleDone, tintColor: colors.orange } :
-          { title: "Edit", handler: props.handleEdit, tintColor: colors.orange }
-      }
-      rightButton={
-        <TouchableOpacity onPress={props.handleAdd}>
-          <Icon name="plus" size={32} color={colors.orange} style={styles.navbarPlus} />
-        </TouchableOpacity>
-      }
-      containerStyle={styles.navbar}
-      statusBar={{ style: 'light-content' }}
-    />
-  )
+class NavBar extends Component {
+  render() {
+    return (
+      <NavigationBar
+        title={{ title: "Hangboard Timer", style: styles.navbarTitle }}
+        leftButton={
+          this.props.currentRoute === 'edit' ?
+            { title: "Done", handler: this.props.handleDone, tintColor: colors.orange } :
+            { title: "Edit", handler: this.props.handleEdit, tintColor: colors.orange }
+        }
+        rightButton={
+          <TouchableOpacity onPress={this.props.handleAdd}>
+            <Icon name="plus" size={32} color={colors.orange} style={styles.navbarPlus} />
+          </TouchableOpacity>
+        }
+        containerStyle={styles.navbar}
+        statusBar={{ style: 'light-content' }}
+      />
+    )
+  }
 }
+
+export default NavBar
